31393efe3c18217d5b38f4f4af98d5c3b57357f6
pylipp <philipp.metzner@tum.de>
2017-12-08 21:08:52 +0100

    Prepare for multiline support.

--- lib/commentbox.py
+++ lib/commentbox.py
@@ -1,30 +1,34 @@
 """Module for creating boxes around text."""
 
 
-def create_box_elements(line, text_width, comment_char=None):
-    """Create modified text line and wrapping lines acc. to given line, text
+def create_box_elements(lines, text_width=80, comment_char=None):
+    """Create modified text line and wrapping line acc. to given line, text
     width and comment character.
     """
     comment_char = comment_char or '#'
-    current_line_without_indent = line.strip()
+    first_line = lines[0]
+    current_line_without_indent = first_line.strip()
     first_char_in_line = current_line_without_indent[0]
-    first_char_col = line.find(first_char_in_line)
-    indent = line[:first_char_col]
+    first_char_col = first_line.find(first_char_in_line)
+    indent = first_line[:first_char_col]
     text_line_length = text_width - first_char_col
     text_line_length_without_comment_chars = text_line_length -\
             2*len(comment_char)
-    return [
-        indent + text_line_length * comment_char,
-        indent + comment_char + current_line_without_indent.center(
-            text_line_length_without_comment_chars) + comment_char,
-        indent + text_line_length * comment_char,
-    ]
-
+    wrapping_line = indent + text_line_length * comment_char
+    elements = [wrapping_line]
+    for line in lines:
+        elements.append(
+            indent + comment_char + line.strip().center(
+            text_line_length_without_comment_chars) + comment_char)
+    return elements
 
 def put_text_in_box(buffer, row, text_width, comment_char=None):
     """Put the text at line `row` in the `buffer` into a comment box."""
-    first_line, text_line, last_line = create_box_elements(
-        buffer[row], text_width, comment_char=comment_char)
-    buffer.append(last_line, row)
-    buffer.append(first_line, row - 1)
-    buffer[row] = text_line
+    elements = create_box_elements([buffer[row]],
+        text_width=text_width, comment_char=comment_char)
+    wrapping_line = elements[0]
+    text_lines = elements[1:]
+    buffer.append(wrapping_line, row)
+    buffer.append(wrapping_line, row - 1)
+    for i, line in enumerate(text_lines):
+        buffer[row + i] = line
--- lib/unit_test.py
+++ lib/unit_test.py
@@ -6,23 +6,21 @@ from commentbox import create_box_elements, put_text_in_box
 class TestCreateBoxElements(unittest.TestCase):
 
     def test_line_without_indent(self):
-        line = "test line"
+        lines = ["test line"]
         expected = [
             "#############",
             "# test line #",
-            "#############",
         ]
-        actual = create_box_elements(line, 13)
+        actual = create_box_elements(lines, text_width=13)
         self.assertListEqual(expected, actual)
 
     def test_line_with_indent(self):
-        line = "    some code  "
+        lines = ["    some code  "]
         expected = [
             "    ##############",
             "    # some code  #",
-            "    ##############",
         ]
-        actual = create_box_elements(line, 18)
+        actual = create_box_elements(lines, text_width=18)
         self.assertListEqual(expected, actual)
 
 
